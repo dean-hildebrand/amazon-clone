8f805adab0e5af5280bda99913ee40c5077264c7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-06 08:47:15 -0500

    Set up firebase and created config file

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQXW6p0EPPFIDHi0E2oDn-DMK5Nhh521o",
+  authDomain: "clone-837be.firebaseapp.com",
+  databaseURL: "https://clone-837be.firebaseio.com",
+  projectId: "clone-837be",
+  storageBucket: "clone-837be.appspot.com",
+  messagingSenderId: "358237655061",
+  appId: "1:358237655061:web:0079bed4b1613fe262fe1c",
+  measurementId: "G-PQ61H4QX2V"
+};
