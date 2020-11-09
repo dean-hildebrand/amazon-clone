e2b1dba72b93e6249a1ef3bc18b25a01e4184bcf
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-09 10:34:10 -0500

    UseState hooks to track email and password onChange

--- amazon-clone/src/components/Login.js
+++ amazon-clone/src/components/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/login.css";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = e => {
+    e.preventDefault();
+    // firebase login user code
+  }
+
+  const register = e => {
+    e.preventDefault();
+    // firebase register user code
+  }
+
   return (
     <div className="login">
       <Link to="/">
@@ -17,12 +30,20 @@ function Login() {
         <h1>Sign-in</h1>
         <form>
           <h5>E-mail</h5>
-          <input type="text" />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
           <h5>Password</h5>
-          <input type="password" />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-          <button className="login__signInButton">Sign In</button>
+          <button type="submit" className="login__signInButton" onClick={signIn}>Sign In</button>
         </form>
         <p>
           By signing in you agree to the AMAZON FAKE CLONE Conditions of Use &
@@ -30,7 +51,7 @@ function Login() {
           Interest-Base Ads Notice. Thank you
         </p>
 
-        <button className="login__registerButton">
+        <button className="login__registerButton" onClick={register}>
           Create an Amazon Account
         </button>
       </div>
