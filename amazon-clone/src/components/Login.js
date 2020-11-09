54228445ae56e820e323c8879dd8c1e4471d94ab
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-09 10:05:16 -0500

    Create input form to sign in on /login route

--- amazon-clone/src/components/Login.js
+++ amazon-clone/src/components/Login.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "../css/login.css";
 
 function Login() {
   return (
     <div className="login">
-    <Link to="/">
-      <img
-      className='login__logo'
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-        alt=""
-      />
+      <Link to="/">
+        <img
+          className="login__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+          alt=""
+        />
       </Link>
+
+      <div className="login__container">
+        <h1>Sign-in</h1>
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" />
+
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By signing in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and out
+          Interest-Base Ads Notice. Thank you
+        </p>
+
+        <button className="login__registerButton">
+          Create an Amazon Account
+        </button>
+      </div>
     </div>
   );
 }
