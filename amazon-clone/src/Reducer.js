d300971c65e20a82a84b7f8d252e713debc35365
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-11-09 09:45:13 -0500

    used reducer for a remove from basket method, spliced item by index

--- amazon-clone/src/Reducer.js
+++ amazon-clone/src/Reducer.js
@@ -5,21 +5,37 @@ export const initialState = {
 // Selector
 //use reduce method to tally up the basket total price depending on the items in the basket
 export const getBasketTotal = (basket) =>
-// iterate the basket and set initial "amount"
-basket?.reduce((amount, item) => item.price + amount, 0);
+  // iterate the basket and set initial "amount"
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case "ADD_TO_BASKET":
       return {
         ...state,
         basket: [...state.basket, action.item],
       };
+    case "REMOVE_FROM_BASKET":
+      // cant filter based on id because it will remove multiple of tha same item if in your cart. Need to use the products index
+      const index = state.basket.findIndex((item) => item.id === action.id);
+      let newBasket = [...state.basket];
 
-      default:
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as it is not in the cart`
+        );
+      }
+      return {
+        ...state,
+        basket: newBasket
+      }
+
+    default:
       return state;
   }
 };
-
 
 export default reducer;
